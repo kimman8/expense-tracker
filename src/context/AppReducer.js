baef1b8262bfd32b6dbe62240f2efd913136e432
kim <kim_man@live.com>
2021-11-03 11:45:53 +1100

    fixed warnings for deploy

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,13 +1,13 @@
-export default (state, action) => {
+const AppReducer = (state, action) => {
   switch (action.type) {
-    case "DELETE_TRANSACTION":
+    case 'DELETE_TRANSACTION':
       return {
         ...state,
         transactions: state.transactions.filter(
           (transaction) => transaction.id !== action.payload
         ),
       };
-    case "ADD_TRANSACTION":
+    case 'ADD_TRANSACTION':
       return {
         ...state,
         transactions: [action.payload, ...state.transactions],
@@ -16,3 +16,4 @@ export default (state, action) => {
       return state;
   }
 };
+export default AppReducer;
